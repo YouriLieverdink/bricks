8a5e2e511b4b8677062fe8948329c050bd235c67
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-07-05 10:47:21 +0200

    feat(flutter_widget): initial version

---
+++ bricks/flutter_widget/__brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,12 @@
+import 'flutter/material.dart';
+
+class {{name.pascalCase()}} extends StatelessWidget {
+  const {{name.pascalCase()}}({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
